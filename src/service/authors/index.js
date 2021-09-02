7550cd4f53bb16511e3f3ef79b3b2caa62c16369
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-02 16:18:46 +0200

    crud for author done

--- src/service/authors/index.js
+++ src/service/authors/index.js
@@ -3,7 +3,7 @@ import authorsModel from "./schema.js";
 
 const authorsRouter = Router();
 
-authorsRouter.post("/", async (req, res, next) => {
+authorsRouter.get("/", async (req, res, next) => {
   try {
     const author = await authorsModel(req.body).save();
     res.send(author._id);
@@ -18,10 +18,53 @@ authorsRouter.get("/", async (req, res, next) => {
     // const query = q2m(req.query);
     // console.log(query);
 
-    // const total = await authorsModel.countDocuments(query.criteria); //will have to finsish the query when i get the posts
+    // const total = await authorsModel.countDocuments(query.criteria); //will have to finsish the query when i get the authors
     const authors = await authorsModel.find();
 
     res.send(authors);
+  } catch (error) {
+    next(error);
+  }
+});
+
+authorsRouter.get("/:Id", async (req, res, next) => {
+  try {
+    const author = await authorsModel.findById(req.params.Id);
+    if (author) {
+      res.send(author);
+    } else {
+      res.send(`blog ${req.params.Id} NOT found!!`);
+    }
+  } catch (error) {
+    next(createHttpError(404, `author ${req.params.Id} NOT found!!`));
+  }
+});
+authorsRouter.put("/:Id", async (req, res, next) => {
+  try {
+    const author = await authorsModel.findByIdAndUpdate(
+      req.params.Id,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    if (author) {
+      res.send(author);
+    } else {
+      res.send(`blog ${req.params.Id} NOT found!!`);
+    }
+  } catch (error) {
+    next(createHttpError(404, `author ${req.params.Id} NOT found!!`));
+  }
+});
+authorsRouter.delete("/:Id", async (req, res, next) => {
+  try {
+    const author = await authorsModel.findByIdAndDelete(req.params.Id);
+    if (author) {
+      res.status(204).send(`Deleted!!`);
+    } else {
+      res.send(`${req.params.Id} NOT found!`);
+    }
   } catch (error) {
     next(error);
   }
