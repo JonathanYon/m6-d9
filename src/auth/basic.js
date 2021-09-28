864e50468f576792c462dc14278a04ea7911438c
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-28 15:12:32 +0200

    hashed pass

---
+++ src/auth/basic.js
@@ -0,0 +1,15 @@
+// import createHttpError from "http-errors"
+// import atob from "atob"
+
+// export const authMidllware = async(req, res, next) => {
+//     try {
+//         if (!req.headers.authorization){
+
+//         } else {
+//             const decodeUserPass = atob(req.headers.authorization.split(" "))
+//             const [email, password] = decodeUserPass.split(":")
+//         }
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
--- src/service/authors/schema.js
+++ src/service/authors/schema.js
@@ -1,9 +1,28 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
+import bcrypt from "bcrypt";
 
-const authorSchema = new Schema({
-  name: { type: String, required: true },
-  avatar: { type: String, required: true },
+const authorSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    avatar: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+authorSchema.pre("save", async function (next) {
+  //this is where we hash the password of our author
+  const currentUser = this; // this in this case is indicating to the newly registered author
+  const normalPassWord = currentUser.password;
+  // currentUser.password = await bcrypt.hash(normalPassWord, 10);
+
+  if (currentUser.isModified("password")) {
+    currentUser.password = await bcrypt.hash(normalPassWord, 10); //this is similar to line 19 but also account for password change or reset or register
+  }
+
+  next();
 });
 
 export default model(`Author`, authorSchema);
