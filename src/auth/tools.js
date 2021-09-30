2cdd9d1d8d6ec0bc2c4daf0a29d51ca57c361535
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-10-01 00:25:00 +0200

    access + refresh token

--- src/auth/tools.js
+++ src/auth/tools.js
@@ -2,12 +2,27 @@ import jwt from "jsonwebtoken";
 import authorModel from "../service/authors/schema.js";
 import createHttpError from "http-errors";
 
+//access token
 const newToken = (payload) =>
   new Promise((resolve, reject) =>
     jwt.sign(
       payload,
       process.env.ACCESS_TOKEN,
-      { expiresIn: "1 hour" },
+      { expiresIn: "3 hour" },
+      (err, token) => {
+        if (err) reject(err);
+        resolve(token);
+      }
+    )
+  );
+
+// refresh token
+const newRefreshToken = (payload) =>
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      process.env.REFRESH_TOKEN,
+      { expiresIn: "1 week" },
       (err, token) => {
         if (err) reject(err);
         resolve(token);
@@ -17,14 +32,8 @@ const newToken = (payload) =>
 
 export const verifyToken = (token) =>
   new Promise((resolve, reject) =>
-    jwt.verify(token, process.env.REFRESH_TOKEN, (err, decodedToken) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decodedToken) => {
       if (err) reject(err);
       resolve(decodedToken);
     })
   );
-
-export const jwtAuthentication = async (user) => {
-  const accessToken = await newToken({ _id: user._id });
-  console.log("toke=>", accessToken);
-  return accessToken;
-};
